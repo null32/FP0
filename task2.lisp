fc9a20cf22fd74287a3a11a7880bf7717f21b932
Pavel <pavelgurwork@gmail.com>
2020-04-23 09:12:49 +0300

    Remake of #5, #9 and #13 in task2

--- task2.lisp
+++ task2.lisp
@@ -1,3 +1,129 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Reworked
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(write-line "")
+;;; #5
+;;; Определить функциональный предикат (any pred list),
+;;; который истенен, если pred истенен для хотя бы
+;;; одного элемента списка list
+;;; (any (lambda (x) (> x 3)) '(1 2 3)) => nil
+;;; (any (lambda (x) (> x 3)) '(1 5 3)) => T
+
+; Version 1
+;(defun any (pred lst)
+;    (reduce (lambda (p x) (or p (funcall pred x))) lst :initial-value nil)
+;)
+
+; Version 3
+; -- "без reduce и init..."
+(defun any (pred lst)
+    (not (null (mapcan (lambda (x) (cond ((funcall pred x) (list x)) (t nil))) lst)))
+)
+
+;;; Test #1
+(write-line "Test 1")
+(princ " >> (any (lambda (x) (> x 3)) '(1 2 3))")
+(print (any (lambda (x) (> x 3)) '(1 2 3)))
+(write-line "")
+
+;;; Test #2
+(write-line "Test 2")
+(princ " >> (any (lambda (x) (> x 3)) '(1 5 3))")
+(print (any (lambda (x) (> x 3)) '(1 5 3)))
+(write-line "")
+
+
+
+(write-line "")
+;;; #9
+;;; Напишите генератор чисел Фибоначчи
+
+; Version 1
+;(defun fib-gen ()
+;    (let
+;        ((a 1) (b 0) (temp 0))
+;        (lambda ()
+;            (progn
+;                (setq temp b)
+;                (setq b (+ a b))
+;                (setq a temp)
+;                b
+;            )
+;        )
+;    )
+;)
+
+; Version 2
+;
+; -- "желательно без prongn"
+; -- да легко
+;
+(defun fib-gen ()
+    (let
+        ((a 1) (b 0))
+        (lambda ()
+            ((lambda (v1 v2)
+                (cond
+                    ((psetq a v1 b v2) nil) ; psetq always returns nil
+                    (t v2)
+                )
+            ) b (+ a b))
+        )
+    )
+)
+
+;;; Test #1
+(write-line "Test 1")
+(princ " first fib generator")
+(setq f1 (fib-gen))
+(print (funcall f1))
+(print (funcall f1))
+(print (funcall f1))
+(write-line "")
+(princ " second fib generator")
+(setq f2 (fib-gen))
+(print (funcall f2))
+(print (funcall f2))
+(print (funcall f2))
+(print (funcall f2))
+(write-line "")
+
+
+
+(write-line "")
+;;; #13
+;;; Определите функцию, которая возвращает своё определение
+(setq getme
+    '(
+        (lambda (x)
+            (list x (list 'quote x))
+        )
+        '(lambda (x)
+            (list x (list 'quote x))
+        )
+    )
+)
+
+;;; Test #1
+(write-line "Test 1")
+(princ " eval function")
+(print (eval getme))
+(write-line "")
+
+;;; Test #2
+(write-line "Test 2")
+(princ " recursively eval function 3 times")
+(print (eval (eval (eval getme))))
+(write-line "")
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Completed
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(write-line "")
 ;;; #1
 ;;; Определить funcall через apply
 ;;; (funcall '+ 1 2 3) == (funcall-2 '+ 1 2 3)
@@ -51,32 +177,6 @@
 
 
 (write-line "")
-;;; #5
-;;; Определить функциональный предикат (any pred list),
-;;; который истенен, если pred истенен для хотя бы
-;;; одного элемента списка list
-;;; (any (lambda (x) (> x 3)) '(1 2 3)) => nil
-;;; (any (lambda (x) (> x 3)) '(1 5 3)) => T
-
-(defun any (pred lst)
-    (reduce (lambda (p x) (or p (funcall pred x))) lst :initial-value nil)
-)
-
-;;; Test #1
-(write-line "Test 1")
-(princ " >> (any (lambda (x) (> x 3)) '(1 2 3))")
-(print (any (lambda (x) (> x 3)) '(1 2 3)))
-(write-line "")
-
-;;; Test #2
-(write-line "Test 2")
-(princ " >> (any (lambda (x) (> x 3)) '(1 5 3))")
-(print (any (lambda (x) (> x 3)) '(1 5 3)))
-(write-line "")
-
-
-
-(write-line "")
 ;;; #7
 ;;; Определить фильтр (where pred list),
 ;;; удаляющий из списка list все елементы,
@@ -96,36 +196,6 @@
 (write-line "Test 2")
 (princ " >> (where (lambda (x) (= (mod x 2) 0)) '(1 2 3 4))")
 (print (where (lambda (x) (= (mod x 2) 0)) '(1 2 3 4)))
-(write-line "")
-
-
-
-(write-line "")
-;;; #9
-;;; Напишите генератор чисел Фибоначчи
-(defun fib-gen ()
-    (let
-        ((a 1) (b 0) (temp 0))
-        (lambda ()
-            (progn
-                (setq temp b)
-                (setq b (+ a b))
-                (setq a temp)
-                b
-            )
-        )
-    )
-)
-
-;;; Test #1
-(write-line "Test 1")
-(princ " first 5 elements of fib sequence")
-(setq f1 (fib-gen))
-(print (funcall f1))
-(print (funcall f1))
-(print (funcall f1))
-(print (funcall f1))
-(print (funcall f1))
 (write-line "")
 
 
@@ -155,21 +225,3 @@
 
 
 
-(write-line "")
-;;; #13
-;;; Определите функцию, которая возвращает своё определение
-(defun getme ()
-    (symbol-function 'getme)
-)
-
-;;; Test #1
-(write-line "Test 1")
-(princ " call function normal way")
-(print (getme))
-(write-line "")
-
-;;; Test #2
-(write-line "Test 2")
-(princ " recursively call function 3 times")
-(print (funcall (funcall (getme))))
-(write-line "")
